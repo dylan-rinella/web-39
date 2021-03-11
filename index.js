b55f592cc7f8d7da229f0983c30bfcff6d75fa4c
dylan-rinella <rinelladylan@aim.com>
2021-03-11 12:48:14 -0500

    pogu

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'development') { //on Heroku machine, an env variable is called "NODE_ENV" --> "production"
   server.use(cors())
 }
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+  console.log(`*** Listening on :${PORT} ***`)
+})
