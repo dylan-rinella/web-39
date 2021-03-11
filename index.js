6c48e0c7200bbb5348f9f1ec78d9ee4c7a0b9660
dylan-rinella <rinelladylan@aim.com>
2021-03-11 12:54:12 -0500

    yes

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ if (process.env.NODE_ENV === 'development') { //on Heroku machine, an env variab
   server.use(cors())
 }
 
+server.use('*', (req, res) => {
+  res.send('<h1>Success</h1>')
+})
+
 const PORT = process.env.PORT || 4000
 
 server.listen(PORT, () => {
